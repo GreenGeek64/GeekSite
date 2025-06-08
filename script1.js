2b1cb3236822ebd500ce3c384d88ef9892beb508
GreenGeek64 <GreenGeeks64@gmail.com>
2025-06-08 15:05:34 +0100

    mess

--- script1.js
+++ script1.js
@@ -13,6 +13,7 @@ const ctx = canvas.getContext("2d");
 const canvasWidth = canvas.getAttribute("width");
 const canvasHeight = canvas.getAttribute("height");
 
+// Define drawline function from start-end (x,y-x,y)
 function drawLine(xStart, yStart, xEnd, yEnd) {
   // Draw a line from start to end position xx
   ctx.beginPath();
@@ -21,6 +22,8 @@ function drawLine(xStart, yStart, xEnd, yEnd) {
   ctx.stroke()
 }
 
-for (let i = 0; i < 2; i++) {
-  drawLine(0+(i*50),0,canvasWidth,canvasHeight-(i*50));
+// width="700" height="450" by default. Draw isometric grid
+for (let i = -9; i < 9; i++) {
+  drawLine(0+(75*i),0,canvasWidth,canvasHeight-(50*i));
+  drawLine(0+(75*i),canvasHeight,canvasWidth,50*i);
 }
