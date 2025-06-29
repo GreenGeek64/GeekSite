08a63a2f21f263b797a02fc2a30d99d24768745c
GreenGeek64 <GreenGeeks64@gmail.com>
2025-06-29 12:29:25 +0100

    Added clickable circles to the canvas, draws a line which does not end where it should

--- script1.js
+++ script1.js
@@ -20,13 +20,51 @@ function drawLine(xStart, yStart, xEnd, yEnd) {
   ctx.moveTo(xStart, yStart);
   ctx.lineTo(xEnd, yEnd);
   ctx.stroke()
+  ctx.closePath()
 }
 
-// Define an object for the notes
-function noteCircle(x, y, noteName) {
-  this.x = x;
-  this.y = y;
-  this.noteName = noteName;
+// Create a circle class with 4 parameters
+class Circle {
+  constructor(xpoint, ypoint, radius, color) {
+    this.xpoint = xpoint;
+    this.ypoint = ypoint;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  // draw on ctx
+  draw(ctx) {
+    // Begin path for drawing, draw an arc x,y,r, no start angle, full circumference, clockwise
+    ctx.beginPath();
+    ctx.arc(this.xpoint, this.ypoint, this.radius, 0, Math.PI * 2, false);
+
+    // Some style 
+    ctx.strokeStyle = 'grey';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = this.color;
+    ctx.fill();
+
+    // Draw + closepath
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  // Use pythag to determine if click is within circle
+  clickCircle(xmouse, ymouse) {
+    const distance = 
+    Math.sqrt(
+    ( ( xmouse - this.xpoint ) ** 2 )
+     + 
+    ( ( ymouse - this.ypoint ) ** 2 )
+    );
+
+    if (distance < this.radius) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
 }
 
 // width="700" height="450" by default. Draw isometric grid
@@ -36,16 +74,20 @@ for (let i = -9; i < 9; i++) {
   drawLine(0+(75*i),canvasHeight,canvasWidth,50*i);
 }
 
+let circle = new Circle(200,200,50,'lavender');
+circle.draw(ctx);
 
-var newNote = document.createElement("BUTTON");
-newNote.className = "note";
-newNote.style.position = "relative"
-newNote.onclick = drawLine(0,0,200,200)
-/* Why is this doing it before the button is clicked ???? */
+// Whenver the screen is clicked, check if a circle is clicked within the canvas frame
+// NEXT: draw a line from the midpoint of the circle to the mouse. Repeat until click again, snap onto new circle
+var lineToCursor = false;
 
-var noteName = document.createTextNode("C");
-newNote.appendChild(noteName);
+canvas.addEventListener('click', (event) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
 
-document.body.appendChild(newNote);
-
-console.log("bsoo")
+  if (circle.clickCircle(x, y)) {
+    lineToCursor = true;
+    drawLine(x,y,event.clientX,event.clientY);
+  }
+} );
